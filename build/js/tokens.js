6dce2b70b88e65697ae4df54f0e89c34962c5e21
diaspd <pdr050203@gmail.com>
2022-11-25 18:37:47 -0300

    firt commit

---
+++ build/js/tokens.js
@@ -0,0 +1,13 @@
+/**
+ * Do not edit directly
+ * Generated on Fri, 25 Nov 2022 21:29:47 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorGray200": "#c1c1c4",
+  "ColorGray500": "#50505c",
+  "ColorGray800": "#1c1c1f",
+  "ColorGray900": "#18181b",
+  "ColorPurple500": "#8257e3"
+};
